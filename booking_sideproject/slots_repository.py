422100eace156e18a975e5b37f47630253f37f05
jonathan131t5 <jona.wexler@gmail.com>
2026-05-06 22:27:49 +0300

    booking 06/05/2026

---
+++ booking_sideproject/slots_repository.py
@@ -0,0 +1,75 @@
+class BookingSlotRepository:
+    def __init__(self , cursor):
+        self.cursor = cursor
+
+
+    def create_booking_table(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS booking_slot(
+        slot_id INTEGER PRIMARY KEY AUTOINCREMENT , 
+        date DATETIME , 
+        is_taken DEFAULT 0
+        )
+        """)
+
+
+    def create_booking_slot(self , date):
+        self.cursor.execute(
+        "INSERT INTO booking_slot (date) VALUES (?)" , 
+        (date , )
+        )
+
+
+    def close_booking_slot(self , slot_id):
+        self.cursor.execute(
+        "UPDATE booking_slot SET is_taken = 1 WHERE slot_id = ?" , 
+        (slot_id , )
+        )
+
+
+    def get_all_slots(self):
+        self.cursor.execute(
+        "SELECT * FROM booking_slot"
+        )
+        rows = self.cursor.fetchall()
+        if rows is None:
+            return None
+        
+        slots = []
+
+        for row in rows:
+            slots.append({
+                "slot_id" : row[0] , 
+                "date" : row[1] , 
+                "is_taken": row[2]
+                })
+            
+        return slots
+    
+
+
+
+
+class AppointmentRepository:
+    def __init__(self , cursor):
+        self.cursor = cursor
+
+
+    
+    def create_appointment_table(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS appointment(
+        id INTEGER PRIMARY KEY AUTOINCREMENT , 
+        slot_id INTEGER , 
+        lead_id INTEGER , 
+        email TEXT , 
+        status DEFAULT 'pending'
+        )
+        """)
+
+
+    def create_appointment(self , slot_id , lead_id , email):
+        self.cursor.execute(
+        "INSERT INTO appointment (slot_id , lead_id , email) VALUES (? , ? , ?)" , 
+        (slot_id , lead_id , email)
+        )
