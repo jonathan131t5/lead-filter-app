0ba484d50cba7d36728afa45c519cc6616112040
jonathan131t5 <jona.wexler@gmail.com>
2026-05-15 20:47:21 +0300

    d

--- service/whatsapp_service.py
+++ service/whatsapp_service.py
@@ -10,6 +10,8 @@ EVOLUTION_API_KEY = os.getenv("EVOLUTION_API_KEY")
 
 
 def send_whatsapp_message(number: str, text: str):
+    number = number.replace("@s.whatsapp.net", "")
+    
     url = f"{EVOLUTION_API_URL}/message/sendText/{EVOLUTION_INSTANCE_NAME}"
 
     headers = {
