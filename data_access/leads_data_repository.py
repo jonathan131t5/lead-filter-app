f4b26aa78221d03aebbb7f6e3174379032993d93
jonathan131t5 <jona.wexler@gmail.com>
2026-05-16 19:54:39 +0300

    .

--- data_access/leads_data_repository.py
+++ data_access/leads_data_repository.py
@@ -19,9 +19,9 @@ class LeadsDataRepository:
         """)
 
     
-    def create_new_lead(self, session_id):
-        self.cursor.execute("INSERT INTO leads_data (session_id) VALUES (?)", 
-        (session_id , )
+    def create_new_lead(self, session_id , name):
+        self.cursor.execute("INSERT INTO leads_data (session_id , name) VALUES (? , ?)", 
+        (session_id , name)
         )
 
         return self.cursor.lastrowid
