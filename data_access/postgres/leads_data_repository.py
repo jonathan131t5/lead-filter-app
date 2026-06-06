e529175f54e726752e43895cdeb8f82d07502194
jonathan131t5 <jona.wexler@gmail.com>
2026-06-06 20:56:20 +0300

    .

--- data_access/postgres/leads_data_repository.py
+++ data_access/postgres/leads_data_repository.py
@@ -243,7 +243,7 @@ class LeadsDataRepository:
             else:
                 fields["booking_flow"] += 1
 
-        if len(set(fields.get)) == 1:
+        if len(set(fields.values())) == 1:
             return "No clear drop-off"
         
         return max(fields, key=fields.get)
